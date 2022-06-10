359db451e482ce9fe982071835a0166e729d28ad
Marieke Wesselkamp <Marieke_Wesselkamp@phy-10-126-167-171.eduroam-phy.privat>
2022-06-10 14:14:55 +0200

    lsq

--- fit_.py
+++ fit_.py
@@ -15,8 +15,8 @@ samples = 1
 its = 50 # Petchey 2015
 init = (1, 0)
 
-timeseries_array_obs, timeseries_array_true = model.ricker_simulate(samples, its, theta_true, init = init, obs_error=False, stoch=False)
-#plot_trajectories(timeseries_array_obs, its)
+timeseries_array_obs, timeseries_array_true = model.ricker_simulate(samples, its, theta_true, init = init,
+                                                                    obs_error=False, stoch=False)
 
 x_train = timeseries_array_obs[0][:30]
 x_test = timeseries_array_obs[0][31:]
@@ -30,15 +30,26 @@ x_test = timeseries_array_obs[0][31:]
 def ricker(N, log_r, phi):
     return np.exp(log_r+np.log(N)-N)*phi
 
-# Randomly initialize parameters
-p0 = np.random.exponential(size=2)
+# function to minimize
+def fun(pars, x, y):
+    res = ricker(x, pars[0], pars[1]) - y
+    return res
+
+# Initialize parameters randomly
+log_r_init = np.random.normal(theta_true['log_r'], 0.5, 1)[0]
+phi_init = np.random.normal(theta_true['phi'], 0.5, 1)[0]
+p0 = [log_r_init, phi_init]
+
 # Set min bound 0 on all coefficients, and set different max bounds for each coefficient
 bounds = (0, [4., 15])
 # Data
 x = x_train[:-1]
 y = x_train[1:]
 
-(log_r, phi) , cov = optim.curve_fit(ricker, x, y, bounds=bounds, p0=p0)
+lsq_solution = optim.least_squares(fun, p0, bounds=bounds, loss = 'soft_l1', args=(x, y))
+log_r = lsq_solution.x[0]
+phi = lsq_solution.x[1]
+
 
 #=======================#
 # Forecast time series  #
@@ -90,13 +101,23 @@ vizualisations.plot_forecast(timeseries_array_obs, historic_mean, ricker_preds2,
 # Evaluate forecasts    #
 #=======================#
 
-# define forecast proficiency measure: MSE
+# 1.  Standard proficiency measure: RMSE
 def rmse(y, y_pred):
     return math.sqrt(np.square(np.subtract(y,y_pred)).mean())
 
-print('Root mean square error:', rmse(timeseries_array_obs[0][31:], historic_mean))
-print('Root mean square error:', rmse(timeseries_array_obs[0][31:], ricker_preds1[:,31:]))
+# Use performance of historic mean as forecast proficiency threshold.
+fpt = rmse(timeseries_array_obs[0][31:], historic_mean)
+print('Root mean square error:', fpt)
 
-# 1. Forecast horizon based on Petchey 2015
-#cor = np.corrcoef(x_test, ricker_preds1[0])
-#pred_skill_treshold = 0.5
+errors = []
+for i in range(ricker_preds1.shape[0]):
+    errors.append(rmse(timeseries_array_obs[0][31:], ricker_preds1[i,31:]))
+print('Root mean square error:',errors)
+
+# Forecast horizon as defined in Petchey 2015:
+# When the mean of the forecast distribution falls below the forecast proficiency threshold.
+
+
+
+# 2. Lyapunov exponent
+
