c9275fab66dbc02eda8f16e6687f4b1725a729b5
Marieke Wesselkamp <Marieke_Wesselkamp@phy-10-126-162-230.eduroam-phy.privat>
2022-07-14 15:03:03 +0200

    added efh functions to class_run

--- class_run.py
+++ class_run.py
@@ -4,6 +4,7 @@ import utils
 import proficiency_metrics
 import vizualisations
 import numpy as np
+import matplotlib.pyplot as plt
 
 #==========================================#
 # The Ricker model for population dynamics #
@@ -42,25 +43,120 @@ simulator.initial = True
 perfect_ensemble_d, perfect_ensemble_derivative_d = simulator.simulate(ensemble_size) # resulting trajectories now slightly differ!
 
 #===============================#
+# The horizon with Quantiles    #
+#===============================#
+
+# Choose a forecast proficiency metric
+# 1. Absolute difference to truth.
+abs_diff, abs_diff_mean = proficiency_metrics.absolute_difference(ts_true, perfect_ensemble_d, mean = True)
+vizualisations.FP_absdifferences(abs_diff, abs_diff_mean, its)
+
+# 2. Rolling MSE
+# Metric Parameter: Moving window
+mse = proficiency_metrics.mean_squared_error(ts_true, perfect_ensemble_d)
+mse_rolling = proficiency_metrics.mean_squared_error_rolling(ts_true, perfect_ensemble_d)
+
+# 3. Rolling Correlation
+# Metric Parameter: Moving window of size 3 (Petchey.)
+corrs = proficiency_metrics.rolling_corrs(ts_true, perfect_ensemble_d, window=3)
+#corrr = np.transpose(np.array(corr))
+fig = plt.figure()
+plt.plot(np.transpose(corrs))
+fig.show()
+
+def efh_mean(profiencies, threshold):
+    """
+    1. Function parameter: threshold.
+    """
+
+    def empCL(x, percent):
+        ex = np.sort(x)[np.floor(percent / 100 * len(x)).astype(int)]
+        return (ex)
+
+    q_lower = [empCL(profiencies[:, i], 45) for i in range(profiencies.shape[1])]
+    q_mid = [empCL(profiencies[:, i], 50) for i in range(profiencies.shape[1])]
+    q_upper = [empCL(profiencies[:, i], 55) for i in range(profiencies.shape[1])]
+
+    efh = np.array([i < threshold for i in profiencies])
+    corrs_mean = corrs.mean(axis=0)
+    min_pred_skill = min(np.arange(profiencies.shape[1])[corrs_mean < threshold])
+
+    return efh, min_pred_skill
+
+efh_corr, efh_corr_min = efh_mean(corrs, 0.5)
+fig = plt.figure()
+plt.pcolor(efh_corr)
+fig.show()
+# We require a second function parameter: The correlation seems to meander around the threshold.
+# So for example: The EFH is the mean time after which the correlations falls below the threshold for at least three time steps in a row.
+# This is super randomly?!
+# So simply summarize this (following the definiton of Petchey:
+# empirical confidence intervalls, or use quantiles - looks very similar.)
+
+print("Mean EFH with rolling correlation: ", efh_corr_min)
+
+# Quantile Horizon
+def efh_quantile(metric, accepted_error, actual_error, timesteps, quantiles = (0.01, 0.99)):
+    """
+    1. Function parameter: What quantiles to use?
+    2. What is the "expected error" (or the one we accept)? Depends on the metric we choose!
+                                                            0.5 for correlation.
+                                                            Currently initial uncertainty for MSE and absDiff
+    """
+    error_metrics = ['mse', 'abs_diff']
+    qu = np.quantile(actual_error, (quantiles[0], quantiles[1]), axis=0)
+    efh = []
+    for i in range(timesteps):
+        if metric in error_metrics:
+            e = not (min(qu[0, i], qu[1, i]) < accepted_error < max(qu[0, i], qu[1, i])) | ((min(qu[0, i], qu[1, i]) < accepted_error) & (max(qu[0, i], qu[1, i]) < accepted_error))
+        elif metric == 'cor':
+            e = (min(qu[0, i], qu[1, i]) < accepted_error < max(qu[0, i], qu[1, i])) | ((min(qu[0, i], qu[1, i]) < accepted_error) & (max(qu[0, i], qu[1, i]) < accepted_error))
+        efh.append(e)
+    min_pred_skill = min(np.arange(len(efh))[efh])
+    return efh, min_pred_skill
+
+
+efh_corrs, efh_corrs_min  = efh_quantile('cor', 0.5, corrs, corrs.shape[1])
+efh_corrs2, efh_corrs2_min = efh_quantile('cor', 0.5, corrs, corrs.shape[1], quantiles=(0.45, 0.55))
+fig = plt.figure()
+plt.plot(efh_corrs)
+plt.plot(efh_corrs2)
+fig.show()
+print("Quantile EFH1 with correlation: ", efh_corrs_min)
+print("Quantile EFH2 with correlation: ", efh_corrs2_min)
+
+
+efh_abs_diff, efh_abs_diff_min = efh_quantile('abs_diff', initial_uncertainty, abs_diff, its)
+fig = plt.figure()
+plt.plot(efh_abs_diff)
+fig.show()
+print("Quantile EFH with absolute differences: ", efh_abs_diff_min)
+
+efh_mse_rolling, efh_mse_rolling_min = efh_quantile('mse', initial_uncertainty, mse_rolling, its)
+fig = plt.figure()
+plt.plot(efh_mse_rolling)
+fig.show()
+print("Quantile EFH with Rolling MSE: ", efh_mse_rolling_min)
+
+# t-test Statistics
+t_stats, p_vals = proficiency_metrics.t_statistic(abs_diff, initial_uncertainty) # Where p-value is smaller than threshold.
+
+
+#===============================#
 # The Lyapunov forecast horizon #
 #===============================#
 
-# Choose a forecast proficiency. Here: Absolute difference to truth.
-abs_diff, abs_diff_mean = proficiency_metrics.absolute_difference(ts_true, perfect_ensemble_d, mean = True)
-vizualisations.FP_absdifferences(abs_diff, abs_diff_mean, its)
-t_stats, p_vals = proficiency_metrics.t_statistic(abs_diff, initial_uncertainty) # Where p-value is smaller than threshold.
-
-# Predictability time with Lyapunov time.
 lyapunovs = dynamics.lyapunovs(perfect_ensemble_derivative_d)
 lyapunovs_stepwise = dynamics.lyapunovs(perfect_ensemble_derivative_d, stepwise=True)
 
-Delta_range = np.linspace(initial_uncertainty, abs_diff.max(), 40)
+Delta_range = np.linspace(initial_uncertainty, abs_diff.max()*2, 40)
 
 predicted_efh = np.array([dynamics.lyapunov_efh(lyapunovs, Delta, initial_uncertainty) for Delta in Delta_range])
 vizualisations.plot_LE_efh_along_Delta(Delta_range, predicted_efh)
 
+
 #=================================================#
-# Forecast horizon follwoing Spring & Ilyina 2018 #
+# Forecast horizon based on  Spring & Ilyina 2018 #
 #=================================================#
 
 # Required: Bootstrap perfect ensemble function:
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
-import math
-from scipy.stats import pearsonr
+
 import numpy as np
 
 def historic_mean(x_test, x_train, length = 'test'):
@@ -10,41 +9,7 @@ def historic_mean(x_test, x_train, length = 'test'):
     historic_var = np.full((length), np.std(x_train), dtype=np.float)
     return historic_mean, historic_var
 
-def rmse(y, y_pred):
-    return math.sqrt(np.square(np.subtract(y,y_pred)).mean())
 
-def forecast_rmse(obs, preds, test_index):
-    """
-    Calculates forecast rmse for a time series of predictions by stepwise adding the next time step.
-    Change this to a moving window? Or pointwise?
-    :param preds: predicted time series
-    :return: time series of rmse
-    """
 
-    forecast_error_distributions = []
-    for i in range(preds.shape[0]):
-        errors = []
-        for j in range(preds[:, test_index:].shape[1] - 1):
-            errors.append(rmse(obs[test_index:test_index+j+1], preds[i,test_index:test_index+j+1]))
-        forecast_error_distributions.append(errors)
-    return forecast_error_distributions
-
-def rolling_corrs(obs, preds, test_index, window = 3):
-    """
-    Rolling correlations between true and predicted dynamics in a moving window.
-    Change to cross-correlation?
-    :param obs: np.vector. true dynamics
-    :param preds: np.array. ensemble predictions.
-    :param test_index: int. Where to start calculation
-    :param window: int. Size of moving window.
-    :return: array with correlations. shape:
-    """
-    forecast_corrs = []
-    for i in range(preds.shape[0]):
-        corrs = []
-        for j in range(test_index, preds.shape[1]-window):
-            corrs.append(pearsonr(obs[j:j+window], preds[i,j:j+window])[0])
-        forecast_corrs.append(corrs)
-    return forecast_corrs
 
 
